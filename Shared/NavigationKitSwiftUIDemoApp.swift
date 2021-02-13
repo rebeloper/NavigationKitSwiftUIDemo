afd0da80808767b4835994a083d257ffc4c7a398
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-13 12:03:09 +0200

    Initial Commit

---
+++ Shared/NavigationKitSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationKitSwiftUIDemoApp.swift
+//  Shared
+//
+//  Created by Alex Nagy on 13.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationKitSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
